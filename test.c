009a6fba4bea1276d5a82f018e33fe3e5df70e08
zhangjun <1922585388@qq.com>
2020-05-26 18:19:09 +0800

    try

---
+++ test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"Utils/yingyong.h"
+void main()
+{
+        int a=10,i=0;
+        for(;i<10;i++)
+        {
+                printf("%d\n",add(i,1));
+        }
+        return;
+}
+
